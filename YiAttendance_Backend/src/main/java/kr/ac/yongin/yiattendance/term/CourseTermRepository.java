75a8e4a2f714a1d91ad6aea0d695fa21008422cd
decidone <decidone@gmail.com>
2021-10-21 18:15:18 +0900

    delete backend

--- YiAttendance_Backend/src/main/java/kr/ac/yongin/yiattendance/term/CourseTermRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.yongin.yiattendance.term;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseTermRepository extends JpaRepository<CourseTerm, String> {
-}
